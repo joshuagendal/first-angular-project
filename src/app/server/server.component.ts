3e1623dcb788083273e9b064ea4fcffd311695c4
Josh Gendal <joshuagendal@Joshuas-MBP.domain>
2017-10-09 19:57:37 -0600

    Added new server component successfully

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'; // must import Component class
+
+@Component({
+  selector: 'app-server', // html tag for component to be referenced by
+  templateUrl: './server.component.html' // name of html file where component will be held
+})
+export class ServerComponent {
+
+}
